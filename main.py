2ba621a6837fdac7380e196eae82483634a9ab83
Julio Cesar Tovar <tovarjuliocesar779@gmail.com>
2024-07-17 16:35:49 -0600

    instructions filled

--- main.py
+++ main.py
@@ -1,12 +1,27 @@
+import platform
+
 from selenium import webdriver
 
 from repositories.model_repository import ModelRepository
 from web_scrapper import WebScrapper
 
-if __name__ == '__main__':
-    driver = webdriver.Edge()
-    model_repository = ModelRepository()
 
+def get_driver():
+    driver_: webdriver
+    match platform.system():
+        case 'Windows':
+            driver_ = webdriver.Edge()
+        case 'Linux':
+            driver_ = webdriver.Chrome()
+        case _:
+            driver_ = webdriver.Safari()
+    return driver_
+
+
+if __name__ == '__main__':
+    driver = get_driver()
+
+    model_repository = ModelRepository()
     web_scrapper = WebScrapper(driver=driver, model_repository=model_repository)
 
     web_scrapper.fetch_instructions_from_directory('instructions')
